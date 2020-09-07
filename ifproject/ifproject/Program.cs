5ed4736f0a700272b9b0dfe05a50450537f2e9aa
Tyra <ekstrandtyra@gmail.com>
2020-09-07 09:31:24 +0200

    händelsespel1

---
+++ ifproject/ifproject/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ifproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string costume;
+            string friend;
+
+            Console.WriteLine("What is your name?");
+
+
+            name = Console.ReadLine();
+            System.Console.WriteLine("Hello " + name);
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("Welcome! You have been invited to the best halloween party ever.Everyone is invited, even you nerd.This is your chance to find the love of your life, so chose wisely.Firstly, what will you be at the party ? A zombie or a mumy?");
+
+            costume = Console.ReadLine();
+
+            if (costume == "zombie" || costume == "Zombie")
+            {
+                Console.WriteLine("Good choice!");
+                Console.WriteLine("You see a witch and a zombie, who do you approach?")
+
+                if (friend == "")
+                {
+
+                }
+
+            }
+
+        }
+
+            else if (costume == "Mumy" || costume == "mumy")
+            {
+                Console.WriteLine("oh wow, risky, hehe, good for you");
+            }
+
+            else
+            {
+                Console.WriteLine("Sorry, I didnt get that, zombie or mumy?");
+            }
+
+Console.ReadLine();
+
+        }
+    }
+}
